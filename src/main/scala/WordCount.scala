2406c3c03a1294c453d6019030914c42c9d22e5e
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-10 13:30:36 +0100

    solution

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -1,16 +1,3 @@
 class WordCount(s: String):
    def countWords: Map[String, Int] =
-      println(s)
-      val l = s
-         .toLowerCase()
-         .replaceAll(" ", "|")
-         .replaceAll(",", "|")
-         .replaceAll(":", "")
-         .replaceAll("\\.", "")
-         .replaceAll("\t", "")
-         .replaceAll("\n", "")
-         .split('|')
-         .toList
-         .filterNot(_ == "")
-      println(l)
-      l.map(w => w -> l.count(_ == w)).toMap
+      "\\w+('?\\w+)*".r.findAllIn(s.toLowerCase).toSeq.groupBy(identity).map((k,v) => k -> v.size)
